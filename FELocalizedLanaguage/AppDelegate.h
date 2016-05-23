f860f4db5f32b4f7c29caf043a2545781b8715a3
FlyElephant <keso_xiang@talkmate.com>
2016-05-23 20:29:03 +0800

    Initial Commit

---
+++ FELocalizedLanaguage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FELocalizedLanaguage
+//
+//  Created by FlyElephant on 16/5/23.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FELocalizedLanaguage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FELocalizedLanaguage
+//
+//  Created by FlyElephant on 16/5/23.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
